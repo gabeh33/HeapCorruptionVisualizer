fdb5c40d5ab839210b25978cc15e7c84af175241
gabeh33 <66029105+gabeh33@users.noreply.github.com>
2022-04-29 14:07:52 -0400

    Adding visualization and corruption detection

--- heapinspect/layout.py
+++ heapinspect/layout.py
@@ -1,6 +1,8 @@
 from heapinspect.auxiliary.ezcolor import color
 from heapinspect.auxiliary.utils import terminal_size
+from colorama import Fore, Back, Style
 
+# from termcolor import colored
 
 class HeapShower(object):
     '''Print heap and arena information in detailed mode.
@@ -10,6 +12,7 @@ class HeapShower(object):
         relative (bool): Show relative addresses.
         w_limit_size (int): Data size limit.
     '''
+
     def __init__(self, hi, relative=False, w_limit_size=8):
         self.hi = hi
         self.record = hi.record
@@ -75,7 +78,7 @@ class HeapShower(object):
             lines.append(self.banner(typ))
             for index in sorted(chunk_dict.keys()):
                 chunks = chunk_dict[index]
-                lines.append(self.banner_index(typ, index+align))
+                lines.append(self.banner_index(typ, index + align))
                 for chunk in chunks:
                     if typ == 'largebins':
                         lines.append(self.large_chunk(chunk))
@@ -84,7 +87,7 @@ class HeapShower(object):
         else:
             lines.append(self.banner('relative ' + typ))
             for index in sorted(chunk_dict.keys()):
-                lines.append(self.banner_index('relative ' + typ, index+align))
+                lines.append(self.banner_index('relative ' + typ, index + align))
                 chunks = chunk_dict[index]
                 for chunk in chunks:
                     if typ == 'largebins':
@@ -135,14 +138,14 @@ class HeapShower(object):
     def relative_addr(self, addr):
         mapname = self.whereis(addr)
         if mapname in self.record.bases:
-            return (mapname, addr-self.record.bases[mapname])
+            return (mapname, addr - self.record.bases[mapname])
         else:
             return ('', addr)
 
     def rela_str(self, addr):
         result = self.relative_addr(addr)
         if result[0]:
-            return result[0]+'+'+hex(result[1])
+            return result[0] + '+' + hex(result[1])
         else:
             return hex(addr)
 
@@ -171,6 +174,7 @@ class PrettyPrinter(object):
         hi (HeapInspector or HeapRecord): HeapInspector or HeapRecord.
         relative (bool): Show relative addresses.
     '''
+
     def __init__(self, hi, relative=False):
         self.hi = hi
         self.record = hi.record
@@ -188,7 +192,7 @@ class PrettyPrinter(object):
         lines = []
         header_fmt = color.green('({size:#x})    fastbins[{index}] ')
         for index in sorted(self.record.fastbins.keys()):
-            size = 2*self.record.size_t * (index+2)
+            size = 2 * self.record.size_t * (index + 2)
             chunks = self.record.fastbins[index]
             tail = ''
             for chunk in chunks:
@@ -208,7 +212,7 @@ class PrettyPrinter(object):
             tail += '<-> ' + color.blue("{:#x} ".format(chunk._addr))
         if tail == '':
             tail = color.blue('None')
-        return head+tail
+        return head + tail
 
     @property
     def smallbins(self):
@@ -217,12 +221,12 @@ class PrettyPrinter(object):
         lines = []
         header_fmt = color.green('({size:#x})    smallbins[{index}] ')
         for index in sorted(self.record.smallbins.keys()):
-            size = 2*self.record.size_t * (index+1)
+            size = 2 * self.record.size_t * (index + 1)
             chunks = self.record.smallbins[index]
             tail = ''
             for chunk in chunks:
                 tail += "<-> " + color.blue("{:#x} ".format(chunk._addr))
-            line = header_fmt.format(size=size, index=index-1) + tail
+            line = header_fmt.format(size=size, index=index - 1) + tail
             if tail != '':
                 lines.append(line)
         return '\n'.join(lines)
@@ -234,16 +238,16 @@ class PrettyPrinter(object):
         lines = []
         header_fmt = color.green('largebins[{index}] ')
         for index in sorted(self.record.largebins.keys()):
-            size = 2*self.record.size_t * (index+1)
+            size = 2 * self.record.size_t * (index + 1)
             chunks = self.record.largebins[index]
             tail = ''
             for chunk in chunks:
                 tail += "<-> " + \
-                    color.blue(
-                        "{:#x}".format(chunk._addr) +
-                        color.green("({:#x}) ".format(chunk.size & ~0b111))
+                        color.blue(
+                            "{:#x}".format(chunk._addr) +
+                            color.green("({:#x}) ".format(chunk.size & ~0b111))
                         )
-            line = header_fmt.format(size=size, index=index-0x3f) + tail
+            line = header_fmt.format(size=size, index=index - 0x3f) + tail
             if tail != '':
                 lines.append(line)
         return '\n'.join(lines)
@@ -255,12 +259,12 @@ class PrettyPrinter(object):
         lines = []
         header_fmt = color.yellow('({size:#x})    entries[{index}] ')
         for index in sorted(self.record.tcache_chunks.keys()):
-            size = 4*self.record.size_t + index*0x10
+            size = 4 * self.record.size_t + index * 0x10
             chunks = self.record.tcache_chunks[index]
             tail = ''
             for chunk in chunks:
                 tail += "-> " + color.blue("{:#x} ".format(
-                    chunk._addr+2*self.record.size_t))
+                    chunk._addr + 2 * self.record.size_t))
             line = header_fmt.format(size=size, index=index) + tail
             if tail != '':
                 lines.append(line)
@@ -280,11 +284,11 @@ class PrettyPrinter(object):
         lines.append(
             color.magenta('top: ') +
             color.blue('{:#x}'.format(self.record.main_arena.top))
-            )
+        )
         lines.append(
             color.magenta('last_remainder: ') +
             color.blue('{:#x}'.format(self.record.main_arena.last_remainder))
-            )
+        )
         lines.append(self.unsortedbins)
         return '\n'.join(lines)
 
@@ -306,3 +310,137 @@ heap_base:{}'''.format(
         w, h = terminal_size()
         return color.__getattr__(color1)(
             '{:=^{width}}'.format('  {}  '.format(msg), width=w))
+
+
+# Got this from https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal-in-python
+# Yay pretty colors!
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class DiagramHeap(HeapShower):
+    """
+    Output the heap information provided by a HeapInspector object in a
+    user-friendly way
+    """
+
+    def __int__(self, hi):
+        super().__init__(hi)
+
+    def draw_chunk(self, chunk, d_up_arrow, d_down_arrow, is_even, draw_red, large_chunk=False):
+        """
+        Draws a single chunk of memory, according to the diagram in the draw_single_bin method
+        """
+        output = ""
+        down_arrow = "\u2193"
+        up_arrow = "\u2191"
+
+        if d_up_arrow:
+            output += f"           {up_arrow}         \n"
+        output += f"    Chunk:0x{chunk._addr:x}     \n"
+        output += "+----------------------------+\n"
+        output += "/ {0:<5} bytes |  A | M |  P  /\n".format(hex(chunk.size))
+        output += "\\----------------------------\\\n"
+        output += "/ FWD Ptr: {0:<18}/\n".format(hex(chunk.fd))
+        output += "\\----------------------------\\\n"
+        output += "/ BCK Ptr: {0:<18}/\n".format(hex(chunk.bk))
+        output += "\\----------------------------\\\n"
+
+        if large_chunk:
+            output += "/ FWD NEXT SIZE: {0:<18}/\n".format(hex(chunk.fd_nextsize))
+            output += "\\----------------------------\\\n"
+            output += "/ BCK NEXT SIZE: {0:<18}/\n".format(hex(chunk.bk_nextsize))
+            output += "\\----------------------------\\\n"
+
+        prev_size = hex(chunk.prev_size)
+        if len(prev_size) > 14:
+            prev_size = prev_size[0:14]
+        output += "/ Prev_size: {0:<16}/\n".format(prev_size)
+        output += "+----------------------------+\n"
+
+        if d_down_arrow:
+            output += f"           {down_arrow}         \n"
+
+        if draw_red:
+            print(Fore.RED + output)
+        elif is_even:
+            print(Fore.GREEN + output)
+        else:
+            print(Fore.LIGHTGREEN_EX + output)
+        print(Style.RESET_ALL)
+
+    def draw_single_bin(self, bin, is_even, large_bin=False, d_up_arrow=True):
+        """
+        Draws fastbin[0] or smallbin[3] or whatever it is given, not all bins of a type
+        Outputs the given memory bin looking something like this
+                            ^
+                            |
+                            |
+                    Address in Memory
+                +--------------------------+
+                /  Size  |  A  |  M  |  P  /
+                \--------------------------\
+                /      Forward Pointer     /
+                \--------------------------\
+               /     Backward Pointer     /
+                \--------------------------\
+                /      Previous Size       /
+                +--------------------------+
+                            |
+                            |
+                          -----
+                           ---
+                            -
+        Will look different for the large bin
+        Bin is a list of chunks
+        """
+
+        addrs_in_bin = [chunk._addr for chunk in bin]
+        corrupted_double_free = not len(addrs_in_bin) == len(set(addrs_in_bin))
+
+
+        for i, chunk in enumerate(bin):
+            d_down_arrow = True
+            if i == len(bin) - 1:
+                d_down_arrow = False
+            self.draw_chunk(chunk, d_up_arrow, d_down_arrow, is_even, corrupted_double_free, large_bin)
+
+    def draw_bin_type(self, bin_dict: dict, type_bin: str, large_bin=False, d_up_arrow=True):
+        """
+        Draws all bins of a given type, such as fastbin or largebin
+        int -> [chunks]
+        """
+
+        if bin_dict.keys():
+            print(f"{bcolors.OKGREEN}" + "-=" * 15 + " " + type_bin + " " + "-=" * 15 + f"{bcolors.ENDC}")
+        else:
+            print(f"{bcolors.OKCYAN}" + "-=" * 15 + " " + type_bin + " " + "-=" * 15 + f"{bcolors.ENDC}")
+
+        for i, bin in enumerate(bin_dict.keys()):
+            addrs_in_bin = [chunk._addr for chunk in bin_dict[bin]]
+            corrupted_double_free = not len(addrs_in_bin) == len(set(addrs_in_bin))
+
+            if corrupted_double_free:
+                print(Fore.RED + f"{type_bin}[{i}]")
+                self.draw_single_bin(bin_dict[bin], True, large_bin, d_up_arrow)
+            elif i % 2 == 0:
+                print(Fore.GREEN + f"{type_bin}[{i}]")
+                self.draw_single_bin(bin_dict[bin], True, large_bin, d_up_arrow)
+            else:
+                print(Fore.LIGHTGREEN_EX + f"{type_bin}[{i}]")
+                self.draw_single_bin(bin_dict[bin], False, large_bin, d_up_arrow)
+
+    def output(self):
+        self.draw_bin_type(self.hi.record.fastbins, "fastbins", d_up_arrow=False)
+        self.draw_bin_type(self.hi.record.unsortedbins, "unsortedbins")
+        self.draw_bin_type(self.hi.record.smallbins, "smallbins")
+        self.draw_bin_type(self.hi.record.largebins, "largebins", True)
+        self.draw_bin_type(self.hi.record.tcache_chunks, "tcachebins", d_up_arrow=False)
